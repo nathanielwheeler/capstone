6eae65785890c3ff4b371eb5808b6c66eeb56eab
Steven P. Kennedy <umbrylax@gmail.com>
2019-09-24 11:54:34 -0600

    added axios (you'll need to npm install it)

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,56 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+import AuthService from './AuthService'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {},
+    groups: [],
+    characters: [],
+    activeGroup: {},
+    currentCharater: {},
+    inventory: [],
+
+
 
   },
   mutations: {
-
+    setUser(state, user) {
+      state.user = user
+    }
   },
   actions: {
-
+    async register({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Register(creds)
+        commit('setUser', user)
+        router.push({ name: "boards" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async login({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Login(creds)
+        commit('setUser', user)
+        router.push({ name: "boards" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async logout({ commit, dispatch }) {
+      try {
+        let success = await AuthService.Logout()
+        if (!success) { }
+        commit('resetState')
+        router.push({ name: "login" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
   }
 })

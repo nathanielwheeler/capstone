8d5ed124edabada5e484944cd2ccddd5baf13b05
dbaldazzi <d.baldazzi@hotmail.com>
2019-09-24 15:32:51 -0600

    added create group modal

--- client/src/components/CreateGroupModal.vue
+++ client/src/components/CreateGroupModal.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="createGroup"></div>
+  <div class="GroupModal">
+    <div id="create-group-modal" class="modal" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Create a Group</h4>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="group name">Group Name</label>
+                <input type="text" class="form-control" id="groupname" placeholder="Group Name"
+                  v-model="newGroup.name" />
+              </div>
+              <div class="form-group">
+                <label for="group name">Group Type</label>
+                <input type="text" class="form-control" id="grouptype" placeholder="Group Type"
+                  v-model="newGroup.type" />
+              </div>
+              <div class="form-group">
+                <label for="group description">Group Description</label>
+                <input type="text" class="form-control" id="groupdescription" placeholder="Group Description"
+                  v-model="newGroup.description" />
+              </div>
+              <button type="submit" @click="createGroup()" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 

538dd9b359fad9bba6cd7fd5896ddb759ac0424f
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-09-30 16:06:42 -0600

    subscribe added, redundant style removed

--- client/src/views/Chat.vue
+++ client/src/views/Chat.vue
@@ -2,7 +2,12 @@
   <div class="chat">
     <div class="container-fluid">
       <div class="row">
-        <h3 class="col-12">{{chat.title}}</h3>
+        <h3 class="col-12">
+          {{chat.title}}
+          <button class="btn btn-primary mybutton" @click="subscribe(chat._id)">
+            <b>Subscribe</b>
+          </button>
+        </h3>
         <div class="messageBox col-12 d-flex flex-column-reverse border" v-chat-scroll>
           <message v-for="message in messages" :messageProp="message" :key="message._id"></message>
         </div>
@@ -15,91 +20,66 @@
 
 
 <script>
-  import message from "../components/Message";
-  import messageInput from "../components/MessageInput";
-  import activeCharacterModal from "../components/ActiveCharacterModal";
+import message from "../components/Message";
+import messageInput from "../components/MessageInput";
+import activeCharacterModal from "../components/ActiveCharacterModal";
 
-  export default {
-    name: "chat",
-    mounted() {
-      let chatId = this.$route.params.chatId;
-      this.$store.dispatch("getChat", chatId);
-      this.$store.dispatch("getMessages", chatId);
+export default {
+  name: "chat",
+  mounted() {
+    let chatId = this.$route.params.chatId;
+    this.$store.dispatch("getChat", chatId);
+    this.$store.dispatch("getMessages", chatId);
+  },
+  props: [],
+  data() {
+    return {};
+  },
+  computed: {
+    chat() {
+      return this.$store.state.currentChat;
     },
-    props: [],
-    data() {
-      return {};
-    },
-    computed: {
-      chat() {
-        return this.$store.state.currentChat;
-      },
-      messages() {
-        return this.$store.state.messages[this.$route.params.chatId];
-      }
-      //   comments() {
-      //   return this.$store.state.comments[this.taskProp._id] || [];
-      // }
-    },
-    methods: {},
-    components: {
-      message,
-      messageInput,
-      activeCharacterModal
+    messages() {
+      return this.$store.state.messages[this.$route.params.chatId];
     }
-  };
+    //   comments() {
+    //   return this.$store.state.comments[this.taskProp._id] || [];
+    // }
+  },
+  methods: {
+    subscribe(chatId) {
+      this.$store.dispatch("subscribe", chatId);
+    }
+  },
+  components: {
+    message,
+    messageInput,
+    activeCharacterModal
+  }
+};
 </script>
 
 
 <style scoped>
-  messageInput {
-    text-align: center;
-    z-index: -1;
+.mybutton {
+  padding: 0px;
+}
+messageInput {
+  text-align: center;
+  z-index: -1;
+}
 
-  }
+.messageBox {
+  height: 450px;
+  overflow-y: auto;
+  overflow-wrap: break-word;
+}
 
-  .messageBox {
-    height: 450px;
-    overflow-y: auto;
-    overflow-wrap: break-word;
-  }
+h3 {
+  text-align: center;
+}
 
-  h3 {
-    text-align: center;
-  }
-
-  .chat {
-    text-align: center;
-  }
-
-  .nav {
-    border-radius: 20%;
-    width: 80px;
-    height: 40px;
-    z-index: 10000;
-  }
-
-  .Current {
-    border-radius: 20%;
-    width: 80px;
-    height: 40px;
-  }
-
-  .Characters {
-    border-radius: 20%;
-    width: 80px;
-    height: 40px;
-  }
-
-  .Group {
-    border-radius: 20%;
-    width: 80px;
-    height: 40px;
-  }
-
-  .LogOut {
-    border-radius: 20%;
-    width: 80px;
-    height: 40px;
-  }
+.chat {
+  text-align: center;
+}
 </style>

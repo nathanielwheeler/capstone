ca6812cfdde5bd3e070f366cac7f4284f3a7fb97
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-09-30 14:38:54 -0600

    removed "Group" button

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
-import Groups from './views/Groups.vue'
+// import Groups from './views/Groups.vue'
 import Chat from './views/Chat.vue'
 import Chats from './views/Chats.vue'
 import Characters from './views/Characters.vue'
@@ -13,54 +13,54 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/groups',
-      name: 'group',
-      component: Groups
-    },
-    {
-      path: '/characters',
-      name: 'characters',
-      component: Characters
-    },
-    {
-      path: '/currentCharacter',
-      name: 'currentCharacter',
-      component: CurrentCharacter
-    },
-    {
-      path: '/chats',
-      name: 'chats',
-      component: Chats
-    },
-    {
-      path: '/chats/:chatId',
-      name: 'chat',
-      component: Chat
-    },
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  // {
+  //   path: '/groups',
+  //   name: 'group',
+  //   component: Groups
+  // },
+  {
+    path: '/characters',
+    name: 'characters',
+    component: Characters
+  },
+  {
+    path: '/currentCharacter',
+    name: 'currentCharacter',
+    component: CurrentCharacter
+  },
+  {
+    path: '/chats',
+    name: 'chats',
+    component: Chats
+  },
+  {
+    path: '/chats/:chatId',
+    name: 'chat',
+    component: Chat
+  },
 
-    {
-      path: "*",
-      redirect: '/login'
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () {
-        return import( /* webpackChunkName: "about" */ './views/About.vue')
-      }
+  {
+    path: "*",
+    redirect: '/login'
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import( /* webpackChunkName: "about" */ './views/About.vue')
     }
+  }
   ]
 })

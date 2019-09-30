e0014b18bae45f11001e5b09145be217a67f04d6
Carson Plant <carsonplant22@gmail.com>
2019-09-30 09:28:05 -0600

    pulled debugger

--- client/src/store.js
+++ client/src/store.js
@@ -210,7 +210,6 @@ export default new Vuex.Store({
       dispatch
     }, chatId) {
       try {
-        debugger
         let res = await api.get("chat/" + chatId + "/messages")
         let data = {
           chatId,

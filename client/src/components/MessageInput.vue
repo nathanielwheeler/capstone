89331458a175c14b3a31b30302f9d7bd5d72640d
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-09-30 15:46:36 -0600

    dropdown for setting active character

--- client/src/components/MessageInput.vue
+++ client/src/components/MessageInput.vue
@@ -2,8 +2,16 @@
   <div class="message-input">
     <form class="grid-container" @submit.prevent="addMessage">
       <span class="dropdown">
-        <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">Voice</button>
-        <div class="dropdown-menu" v-for="character in characters" :key="character._id"></div>
+        <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+          <span v-if="activeCharacter != {}">(None)</span>
+          <span v-else>{{activeCharacter.name}}</span>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" @click="resetActiveCharacter">(None)</a>
+          <div v-for="character in characters" :key="character._id">
+            <a class="dropdown-item" @click="changeActiveCharacter">{{character.name}}</a>
+          </div>
+        </div>
       </span>
       <input class="grid-input" type="text" placeholder="message" v-model="message.body" required />
       <div class="grid-button">
@@ -23,84 +31,74 @@
 
 
 <script>
-  // import ActiveCharacter from "./";
-  import activeCharacterModal from "./ActiveCharacterModal";
-  export default {
-    name: "message-input",
-    data() {
-      return {
-        message: {
-          body: ""
-        }
-      };
-    },
-    mounted() {
-      return this.$route.params.chatId;
-      return this.$store.dispatch("getCharacters");
-    },
-    computed: {
-      user() {
-        return this.$store.state.user;
-      },
-      characters() {
-        return this.$store.state.characters;
+// import ActiveCharacter from "./";
+export default {
+  name: "message-input",
+  data() {
+    return {
+      message: {
+        body: ""
       }
+    };
+  },
+  mounted() {
+    return this.$route.params.chatId;
+    return this.$store.dispatch("getCharacters");
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
     },
-    methods: {
-      addMessage() {
-        let message = {
-          body: this.message.body,
-          //author: this.user._id,
-          chat: this.$route.params.chatId
-        };
-      },
-      mounted() {
-        let chatId = this.$route.params.chatId;
-      },
-      computed: {
-        user() {
-          return this.$store.state.user;
-        }
-      },
-      methods: {
-        addMessage() {
-          let message = {
-            body: this.message.body,
-            //author: this.user._id,
-            chat: this.$route.params.chatId
-          };
-          this.$store.dispatch("addMessage", message);
-          this.message = {};
-        }
-      },
-      components: {
-        activeCharacterModal
-      }
+    characters() {
+      return this.$store.state.characters;
+    },
+    activeCharacter() {
+      return this.$store.state.activeCharacter;
     }
-  }
+  },
+  methods: {
+    addMessage() {
+      let message = {
+        body: this.message.body,
+        //author: this.user._id,
+        chat: this.$route.params.chatId
+      };
+      this.$store.dispatch("addMessage", message);
+      this.message = {};
+    },
+    resetActiveCharacter() {
+      this.$store.dispatch("resetActiveCharacter");
+      this.activeCharacter = {};
+    },
+    changeActiveCharacter() {
+      let characterId = this.character._id;
+      this.$store.dispatch("changeActiveCharacter", characterId);
+    }
+  },
+  components: {}
+};
 </script>
 
 
 <style scoped>
-  .grid-container {
-    display: grid;
-    grid-template-columns: 80px 1fr 65px;
-    grid-template-rows: auto;
-    grid-template-areas: "dropdown input button";
-  }
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 65px;
+  grid-template-rows: auto;
+  grid-template-areas: "dropdown input button";
+}
 
-  .grid-dropdown {
-    grid-area: dropdown;
-    max-width: 80px;
-  }
+.grid-dropdown {
+  grid-area: dropdown;
+}
 
-  .grid-input {
-    grid-area: input;
-    width: 100%;
-  }
+.grid-input {
+  grid-area: input;
+  width: 100%;
+}
 
-  .grid-button {
-    grid-area: button;
-    max-width: 65px;
-  }
+.grid-button {
+  grid-area: button;
+  max-width: 65px;
+}
 </style>

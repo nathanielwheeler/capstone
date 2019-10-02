807447c166dc69110ceef099974571e33779cc41
Steven P. Kennedy <umbrylax@gmail.com>
2019-10-02 11:39:14 -0600

    can edit active character in chat

--- client/src/components/ActiveCharacterModal.vue
+++ client/src/components/ActiveCharacterModal.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="activeCharacter">
     <br />
-    <button class="btn btn-info bottom-margin" data-toggle="modal" data-target="#activeCharacterModal">Current Active
-      Character!</button>
+    <button
+      class="btn btn-info bottom-margin"
+      data-toggle="modal"
+      data-target="#activeCharacterModal"
+    >
+      Current Active
+      Character!
+    </button>
     <div id="activeCharacterModal" class="modal" role="dialog">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -23,8 +29,70 @@
             <editStyle v-if="displayStyle === true" v-bind:character="activeCharacter"></editStyle>
           </div>
           <div class="modal-footer">
-            <button type="button" @click="toggleStyle(displayStyle)" class="btn btn-info border">Edit Style</button>
-            <button type="submit" @click="editcharacter()" class="btn btn-info">Edit Character</button>
+            <button
+              type="button"
+              @click="toggleStyle(displayStyle)"
+              class="btn btn-info border"
+            >Edit Style</button>
+            <div>
+              <button
+                class="btn btn-dark mx-2"
+                data-toggle="modal"
+                :data-target="'#editCharacterModal'+activeCharacter._id"
+              >Edit</button>
+
+              <div :id="'editCharacterModal'+activeCharacter._id" class="modal" role="dialog">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal"></button>
+                      <h4 class="modal-title">Edit your character</h4>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <div class="form-group">
+                          <label for="group name">Character Name</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="charactername"
+                            placeholder
+                            v-model="activeCharacter.name"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="group name">Character Stats</label>
+                          <textarea
+                            class="form-control"
+                            id="characterStats"
+                            placeholder
+                            v-model="activeCharacter.stats"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="character class">Character Description</label>
+                          <textarea
+                            class="form-control"
+                            aria-label="With textarea"
+                            id="characterDescription"
+                            v-model="activeCharacter.description"
+                          ></textarea>
+
+                          <button
+                            type="submit"
+                            @click="editCharacter(activeCharacter)"
+                            class="btn btn-primary"
+                          >Submit</button>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             <button type="button" class="btn btn-danger border" data-dismiss="modal">Close</button>
           </div>
         </div>
@@ -35,48 +103,52 @@
 
 
 <script>
-  import EditStyle from "./EditStyle";
-  export default {
-    name: "Active-Character-Modal",
-    data() {
-      return {
-        character: {},
-        displayStyle: false
-      };
+import EditStyle from "./EditStyle";
+
+export default {
+  name: "Active-Character-Modal",
+  data() {
+    return {
+      character: {},
+      displayStyle: false
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
     },
-    computed: {
-      user() {
-        return this.$store.state.user;
-      },
-      activeCharacter() {
-        return this.$store.state.activeCharacter;
-      }
-    },
-    methods: {
-      getActiveCharacter() {
-        this.activeCharacter.authorId = user._id;
-        this.$store.dispatch("getactiveCharacter", this.activeCharacter);
-        this.activeCharacter = {};
-      },
-      toggleStyle(x) {
-        console.log(x);
-        switch (x) {
-          case false:
-            return (this.displayStyle = true);
-          case true:
-            return (this.displayStyle = false);
-        }
-      }
-    },
-    components: {
-      EditStyle
+    activeCharacter() {
+      return this.$store.state.activeCharacter;
     }
-  };
+  },
+  methods: {
+    getActiveCharacter() {
+      this.activeCharacter.authorId = user._id;
+      this.$store.dispatch("getactiveCharacter", this.activeCharacter);
+      this.activeCharacter = {};
+    },
+    editCharacter(character) {
+      this.$store.dispatch("editCharacter", character);
+    },
+    toggleStyle(x) {
+      console.log(x);
+      switch (x) {
+        case false:
+          return (this.displayStyle = true);
+        case true:
+          return (this.displayStyle = false);
+      }
+    }
+  },
+  components: {
+    EditStyle
+  }
+};
 </script>
 
 
 <style scoped>
-  .bottom-margin {
-    margin-bottom: 75px;
-  }
+.bottom-margin {
+  margin-bottom: 75px;
+}
 </style>

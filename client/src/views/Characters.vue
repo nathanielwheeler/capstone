d70fea0a5aa04a16392d05531c4a61d6e244355d
Carson Plant <carsonplant22@gmail.com>
2019-10-02 11:16:41 -0600

    revert

--- client/src/views/Characters.vue
+++ client/src/views/Characters.vue
@@ -2,9 +2,7 @@
   <div class="characters container-fluid">
     <div class="row">
       <div class="col-12">
-        <strong>
-          <h3 class="offset-3 col-6 text-center">My Characters</h3>
-        </strong>
+        <h3 class="offset-3 col-6 text-center">My Characters</h3>
       </div>
     </div>
     <div class="row">
@@ -14,34 +12,35 @@
       </div>
       <br>
       <br>
-      <strong> 
-      <div class="editcharacter col-12">
-        <div v-for="character in characters" :key="character._id">
-          <strong>
-            <router-link :to="{name:'characters' , params: {characterId: character._id}}" class="route-color">
-              {{character.name}}</router-link>
-          </strong>
+      <strong>
+        <div class="editcharacter col-12">
+          <div v-for="character in characters" :key="character._id">
+            <strong>
+              <router-link :to="{name:'characters' , params: {characterId: character._id}}" class="route-color">
+                {{character.name}}</router-link>
+            </strong>
 
-          <button class="btn btn-dark mx-2" data-toggle="modal"
-            :data-target="'#editCharacterModal'+character._id">Edit</button>
+            <button class="btn btn-dark mx-2" data-toggle="modal"
+              :data-target="'#editCharacterModal'+character._id">Edit</button>
 
-          <div :id="'editCharacterModal'+character._id" class="modal" role="dialog">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal"></button>
-                  <h4 class="modal-title">Edit your character</h4>
-                </div>
-                <div class="modal-body">
-                  <form>
-                    <div class="form-group">
-                      <label for="group name">Character Name</label>
-                      <input type="text" class="form-control" id="charactername" placeholder v-model="character.name" />
-                    </div>
-                    <div class="form-group">
-                      <label for="group name">Character Stats</label>
-                      <textarea class="form-control" id="characterStats" placeholder v-model="character.stats" />
+            <div :id="'editCharacterModal'+character._id" class="modal" role="dialog">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"></button>
+                    <h4 class="modal-title">Edit your character</h4>
+                  </div>
+                  <div class="modal-body">
+                    <form>
+                      <div class="form-group">
+                        <label for="group name">Character Name</label>
+                        <input type="text" class="form-control" id="charactername" placeholder
+                          v-model="character.name" />
                       </div>
+                      <div class="form-group">
+                        <label for="group name">Character Stats</label>
+                        <textarea class="form-control" id="characterStats" placeholder v-model="character.stats" />
+                        </div>
                     <div class="form-group">
                       <label for="character class">Character Description</label>
                       <textarea
@@ -50,27 +49,27 @@
                         id="characterDescription"
                         v-model="character.description"
                       ></textarea>
-                      <!-- <input
+                        <!-- <input
                         type="text"
                         class="form-control"
                         id="characterDescription"
                         placeholder
                         v-model="character.description"
                       />-->
-                      <button type="submit" @click="editCharacter(character)" class="btn btn-primary">Submit</button>
-                    </div>
-                  </form>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" @click="editCharacter(character)" class="btn btn-primary">Submit</button>
+                      </div>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div> 
-          <button class="btn btn-danger" type="button" @click="deleteCharacter(character._id)">Delete</button>
+            <button class="btn btn-danger" type="button" @click="deleteCharacter(character._id)">Delete</button>
+          </div>
         </div>
-      </div> 
-      </strong> 
+      </strong>
     </div>
   </div>
 </template>
@@ -78,59 +77,60 @@
 
 
 <script>
-import CreateCharacterModal from "../components/CreateCharacterModal.vue";
-import ActiveCharacterModal from "../components/ActiveCharacterModal.vue";
+  import CreateCharacterModal from "../components/CreateCharacterModal.vue";
+  import ActiveCharacterModal from "../components/ActiveCharacterModal.vue";
 
-export default {
-  name: "characters",
-  prop: ["characterProp"],
-  data() {
-    return {
-      newCharacter: ""
-    };
-  },
-  mounted() {
-    this.$store.dispatch("getCharacters");
-  },
-  computed: {
-    characters() {
-      return this.$store.state.characters;
-    }
-  },
-  methods: {
-    getCharacters() {
-      this.$store.dispatch("getActiveCharacter", this.activeCharacter);
-      this.activeCharacter = { name: "", stats: "", description: "" };
+  export default {
+    name: "characters",
+    prop: ["characterProp"],
+    data() {
+      return {
+        newCharacter: ""
+      };
     },
-
-    createCharacter() {
-      this.$store.dispatch("createCharacter", this.newCharacter);
-      this.newCharacter = { name: "", stats: "", description: "" };
+    mounted() {
+      this.$store.dispatch("getCharacters");
     },
-
-    deleteCharacter(characterId) {
-      let x = confirm("Are you sure you want to delete this character?");
-      if (x == true) {
-        this.$store.dispatch("deleteCharacter", characterId);
+    computed: {
+      characters() {
+        return this.$store.state.characters;
       }
     },
-    editCharacter(character) {
-      this.$store.dispatch("editCharacter", character);
+    methods: {
+      getCharacters() {
+        this.$store.dispatch("getActiveCharacter", this.activeCharacter);
+        this.activeCharacter = { name: "", stats: "", description: "" };
+      },
+
+      createCharacter() {
+        this.$store.dispatch("createCharacter", this.newCharacter);
+        this.newCharacter = { name: "", stats: "", description: "" };
+      },
+
+      deleteCharacter(characterId) {
+        let x = confirm("Are you sure you want to delete this character?");
+        if (x == true) {
+          this.$store.dispatch("deleteCharacter", characterId);
+        }
+      },
+      editCharacter(character) {
+        this.$store.dispatch("editCharacter", character);
+      }
+    },
+    components: {
+      CreateCharacterModal,
+      ActiveCharacterModal
     }
-  },
-  components: {
-    CreateCharacterModal,
-    ActiveCharacterModal
-  }
-};
+  };
 </script>
 
 
 <style scoped>
-.route-color {
-  color: #3a3a3a;
-}
-.editcharacter {
-  text-align: end;
-}
+  .route-color {
+    color: #3a3a3a;
+  }
+
+  .editcharacter {
+    text-align: end;
+  }
 </style>

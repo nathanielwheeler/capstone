83e5d0fec212f816cc1761ce47bd45dc8541ed29
Carson Plant <carsonplant22@gmail.com>
2019-10-02 09:23:42 -0600

    subscribed chats unsubscribe button

--- client/src/components/Subscribed.vue
+++ client/src/components/Subscribed.vue
@@ -5,12 +5,8 @@
       <div class="col-12">
         <div class="row justify-content-center">
           <router-link :to="{name:'chat' , params: {chatId: chat._id}}" class="col-6">{{chat.title}}</router-link>
-          <button
-            type="button"
-            class="btn btn-danger col-1 mybutton"
-            @click="unsubscribe(chat._id)"
-          >
-            <b>-</b>
+          <button type="button" class="btn btn-danger  mybutton" @click="unsubscribe(chat._id)">
+            <b>Unsubscribe</b>
           </button>
         </div>
       </div>
@@ -21,31 +17,31 @@
 
 
 <script>
-export default {
-  name: "subscribedChats",
-  mounted() {
-    this.$store.dispatch("getSubscribedChats");
-  },
-  data() {
-    return {};
-  },
-  computed: {
-    chats() {
-      return this.$store.state.subscribedChats;
-    }
-  },
-  methods: {
-    unsubscribe(chatId) {
-      this.$store.dispatch("unsubscribe", chatId);
-    }
-  },
-  components: {}
-};
+  export default {
+    name: "subscribedChats",
+    mounted() {
+      this.$store.dispatch("getSubscribedChats");
+    },
+    data() {
+      return {};
+    },
+    computed: {
+      chats() {
+        return this.$store.state.subscribedChats;
+      }
+    },
+    methods: {
+      unsubscribe(chatId) {
+        this.$store.dispatch("unsubscribe", chatId);
+      }
+    },
+    components: {}
+  };
 </script>
 
 
 <style scoped>
-.mybutton {
-  padding: 0px;
-}
+  .mybutton {
+    padding: 0px;
+  }
 </style>
